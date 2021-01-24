ad59254d820b208531d408a1a4d3dbbc0440005e
petrov <a.sav210@gmail.com>
2021-01-24 17:03:49 +0300

    HelloWorld

---
+++ ok-202012-phonesell-pa-helloworld/src/main/kotlin/ru/otus/otuskotlin/phonesell/pa/helloworld/HelloWorld.kt
@@ -0,0 +1,5 @@
+package ru.otus.otuskotlin.phonesell.pa.helloworld
+
+fun main() {
+    println ("Hello World")
+}
---
+++ ok-202012-phonesell-pa-helloworld/src/test/kotlin/ru/otus/otuskotlin/phonesell/pa/helloworld/HelloWorldKtTest.kt
@@ -0,0 +1,10 @@
+package ru.otus.otuskotlin.phonesell.pa.helloworld
+
+import org.junit.Assert.*
+import kotlin.test.Test
+
+class HelloWorldKtTest{
+    @Test
+    fun someTest(){
+    }
+}
