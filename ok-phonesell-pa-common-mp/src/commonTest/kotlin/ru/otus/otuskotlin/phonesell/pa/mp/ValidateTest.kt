6b6edee8b17182a571540703b95eb415249bddae
petrov <a.sav210@gmail.com>
2021-02-09 21:37:08 +0300

    Task 2 ( refactoring validation)

--- ok-phonesell-pa-common-mp/src/commonTest/kotlin/ru/otus/otuskotlin/phonesell/pa/mp/ValidateTest.kt
+++ ok-phonesell-pa-common-mp/src/commonTest/kotlin/ru/otus/otuskotlin/phonesell/pa/mp/ValidateTest.kt
@@ -1,7 +1,5 @@
 package ru.otus.otuskotlin.phonesell.pa.mp
 
-import kotlin.math.max
-import kotlin.math.min
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
@@ -28,32 +26,66 @@ class ValidateTest {
           res.errors.map {it.message}.first().contains("range")
         }
     }
+    @Test
+    fun  validatorListTest(){
 
+        val child=Child (age=10,name="")
+        val validator =ValidatorChild( )
+        val res=validator.validate(child)
+        assertEquals(false,res.isSuccess)
+        assertTrue("contains empty error "){
+            res.errors.filter {it.message.contains("empty")}.isNotEmpty()
+        }
+        assertTrue("contains range error "){
+            res.errors.filter {it.message.contains("empty")}.isNotEmpty()
+        }
+    }
+
+}
+data class Child (
+    val age:Int,
+    val name:String
+)
+class ValidatorChild:IValidator<Child> {
+    val validators: List<IValidator<*>> = listOf(
+        ValidatorInRange("age",2,5),
+        ValidatorStringNotEmpty()
+    )
+    override fun validate(sample: Child): ValidationResult= ValidationResult(
+        (validators[0] as ValidatorInRange<Int>).validate(sample.age),
+        (validators[1] as ValidatorStringNotEmpty).validate(sample.name)
+    )
+}
+
+interface IValidator <T>{
+    fun validate(sample: T): ValidationResult
 }
 
 private fun <T: Comparable<T>> T.validate(validator: ValidatorInRange<T>) =validator.validate(this)
 
 
-class ValidatorInRange<T: Comparable<T>>(val field: String, val min: T, val max: T) {
-        fun validate(sample: T): ValidationResult =if (sample in min..max) {
+class ValidatorInRange<T: Comparable<T>>(val field: String, val min: T, val max: T):IValidator<T> {
+        override fun validate(sample: T): ValidationResult =if (sample in min..max) {
             ValidationResult.SUCCESS
         }else{
             ValidationResult(
                 errors = listOf(
-                    ValidationError(
-                        message = "Field $field with value $sample mast have value of range [$min,$max]"
+                    ValidationFieldError(
+                        field=field,
+                        message = "Field $field with value $sample mast have value of range [$min,$max]",
                     )
                 )
             )
         }
 }
 
-class ValidatorStringNotEmpty {
-    fun validate(sample: String): ValidationResult =if (sample.isNullOrBlank()){
+class ValidatorStringNotEmpty: IValidator<String> {
+    override fun validate(sample: String): ValidationResult =if (sample.isNullOrBlank()){
             ValidationResult(
                 errors = listOf(
-                    ValidationError(
-                        message ="String \"$sample\" must not be empty or null"
+                    ValidationDefaultError(
+                        message ="String \"$sample\" must not be empty or null",
+                        //field = field,
                     )
                 )
             )
@@ -65,18 +97,33 @@ class ValidatorStringNotEmpty {
 fun String.validate(validator: ValidatorStringNotEmpty)=validator.validate(this)
 
 class ValidationResult (
-    val errors: List<ValidationError>
+    val errors: List<IValidationError>
     ) {
-        val isSuccess :Boolean
-            get()=errors.isEmpty() 
+    constructor(vararg errors: ValidationResult) : this(errors.flatMap { it.errors}.toList())
+
+    val isSuccess :Boolean
+            get()=errors.isEmpty()
         companion object{
-        val SUCCESS=ValidationResult(errors = emptyList())    
+        val SUCCESS=ValidationResult(errors = emptyList())
         }   
     }
 
-data class ValidationError (
-    val message :String
-    ){
+data class ValidationDefaultError(
+    override val message: String,
+    ) :IValidationError {
+}
+data class ValidationFieldError(
+    override val message: String,
+    override val field: String,
+) :IValidationFieldError {
+}
+
+interface IValidationError {
+    val message:String
+}
+
+interface IValidationFieldError:IValidationError  {
+    val field: String
 }
 
 
