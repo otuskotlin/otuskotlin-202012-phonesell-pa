e36fd14e80fa172f24ef48cbaab11398e0cfc53e
petrov <a.sav210@gmail.com>
2021-03-12 23:39:21 +0300

    задание №3(начало)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,13 +3,24 @@ rootProject.name = "otuskotlin-phonesell-pa"
 
 
 pluginManagement {
-    plugins {
-        val kotlinVersion: String by settings
+    val kotlinVersion: String by settings
+    val openapiVersion: String by settings
+    val springDependencyVersion: String by settings
+    val springBootVersion: String by settings
+    val bmuschkoVersion: String by settings
+    val kotlessVersion: String by settings
 
-        kotlin("jvm") version kotlinVersion apply false
-        kotlin("js") version kotlinVersion apply false
-        kotlin("multiplatform") version kotlinVersion apply false
-        kotlin("plugin.serialization") version kotlinVersion apply false
+    plugins {
+        kotlin("multiplatform") version kotlinVersion
+        kotlin("jvm") version kotlinVersion
+        kotlin("js") version kotlinVersion
+        kotlin("plugin.serialization") version kotlinVersion
+
+        //id("org.openapi.generator") version openapiVersion
+        //id("org.springframework.boot") version springBootVersion
+        //id("io.spring.dependency-management") version springDependencyVersion
+        id("com.bmuschko.docker-java-application") version bmuschkoVersion
+        //id("io.kotless") version kotlessVersion apply false
     }
 }
 
@@ -17,3 +28,4 @@ include("ok-phonesell-pa-common-mp")
 include("ok-phonesell-pa-common-be")
 include("ok-phonesell-pa-transport-mp")
 include("ok-phonesell-pa-transport-mappers")
+include("ok-phonesell-pa-app-ktor")
