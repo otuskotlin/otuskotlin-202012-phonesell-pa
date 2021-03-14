43e89edf584758dccba8aa85c83ab4b15fb98a0d
petrov <a.sav210@gmail.com>
2021-03-15 00:29:45 +0300

    задание №3(продолжение)

--- ok-phonesell-pa-transport-mp/src/commonMain/kotlin/ru/otus/otuskotlin/phonesell/pa/transport/models/demands/MpRequestDemandUpdate.kt
+++ ok-phonesell-pa-transport-mp/src/commonMain/kotlin/ru/otus/otuskotlin/phonesell/pa/transport/models/demands/MpRequestDemandUpdate.kt
@@ -13,7 +13,7 @@ data class MpRequestDemandUpdate(
     override val requestId: String?=null,
     override val onResponse: String?=null,
     override val startTime: String?=null,
-    override val debug: IMpDebug?=null,
+    override val debug: Debug?=null,
     val updateData: MpDemandUpdateDto? = null,
 ):IMpRequest,MpMessage(){
     @Serializable
