94eb7e71659aeb86453fd049ac1d6599d59cb558
petrov <a.sav210@gmail.com>
2021-03-14 23:57:39 +0300

    задание №3(продолжение)

--- ok-phonesell-pa-transport-mp/src/commonMain/kotlin/ru/otus/otuskotlin/phonesell/pa/transport/models/demands/DemandProductsDto.kt
+++ ok-phonesell-pa-transport-mp/src/commonMain/kotlin/ru/otus/otuskotlin/phonesell/pa/transport/models/demands/DemandProductsDto.kt
@@ -4,8 +4,8 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class DemandProductsDto (
-    val id:        String? = null,
+    val id:        String?=null,
     val idProduct: String?=null,
     val quantity:  String?=null,
-    val price: String?=null,
+    val price:     String?=null,
 )
