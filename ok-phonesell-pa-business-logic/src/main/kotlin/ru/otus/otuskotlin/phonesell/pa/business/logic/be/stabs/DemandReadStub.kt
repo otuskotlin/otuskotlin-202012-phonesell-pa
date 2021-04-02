b09e8369f705ee3ecba9e1fab91c1300c55c73dc
petrov <a.sav210@gmail.com>
2021-04-02 23:35:38 +0300

    Домашнее задание N4

--- ok-phonesell-pa-business-logic/src/main/kotlin/ru/otus/otuskotlin/phonesell/pa/business/logic/be/stabs/DemandReadStub.kt
+++ ok-phonesell-pa-business-logic/src/main/kotlin/ru/otus/otuskotlin/phonesell/pa/business/logic/be/stabs/DemandReadStub.kt
@@ -2,9 +2,7 @@ package ru.otus.otuskotlin.phonesell.pa.business.logic.be.stabs
 
 import ru.otus.otuskotlin.phonesell.pa.common.be.context.MpBeContext
 import ru.otus.otuskotlin.phonesell.pa.common.be.context.MpBeContextStatus
-import ru.otus.otuskotlin.phonesell.pa.common.be.models.MpDemandIdModel
-import ru.otus.otuskotlin.phonesell.pa.common.be.models.MpDemandModel
-import ru.otus.otuskotlin.phonesell.pa.common.be.models.MpStubCase
+import ru.otus.otuskotlin.phonesell.pa.common.be.models.*
 import ru.otus.otuskotlin.phonesell.pa.pipelines.kmp.IOperation
 import ru.otus.otuskotlin.phonesell.pa.pipelines.kmp.operation
 import ru.otus.otuskotlin.phonesell.pa.pipelines.kmp.pipeline
@@ -19,7 +17,22 @@ object DemandReadStub:IOperation<MpBeContext> by pipeline ({
                 id = MpDemandIdModel("test-id"),
                 firstName = "Alexandr",
                 lastName = "Petrov",
-                contactPhone="+79213245777"
+                contactPhone="+79213245777",
+                email="a.sav210@gmail.com",
+                products= mutableSetOf(
+                    MpDemandProductsModel(
+                        id = MpDemandProductsIdModel ("id1") ,
+                        idProduct ="idProduct-1",
+                        quantity = 2,
+                        price = 50000.toBigDecimal(),
+                    ),
+                    MpDemandProductsModel(
+                        id = MpDemandProductsIdModel ("Demand-2"),
+                        idProduct ="idPrdouct-2",
+                        quantity = 2,
+                        price = 50000.toBigDecimal(),
+                    ),
+                )
             )
             status = MpBeContextStatus.FINISHING
         }
--- ok-phonesell-pa-common-be/src/main/kotlin/ru/otus/otuskotlin/phonesell/pa/common/be/models/MpDemandProductsModel.kt
+++ ok-phonesell-pa-common-be/src/main/kotlin/ru/otus/otuskotlin/phonesell/pa/common/be/models/MpDemandProductsModel.kt
@@ -3,7 +3,7 @@ package ru.otus.otuskotlin.phonesell.pa.common.be.models
 import java.math.BigDecimal
 
 data class MpDemandProductsModel(
-    val id:  MpDemandProductsIdModel = MpDemandProductsIdModel.NONE,
+    val id: MpDemandProductsIdModel = MpDemandProductsIdModel.NONE,
     val idProduct: String = "",
     val quantity:  Int = 0,
     val price: BigDecimal = BigDecimal.ZERO,
